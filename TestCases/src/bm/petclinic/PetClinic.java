a0f7d117d7e62955663310e248d4a0941fac6302
mayureshudas <mayuresh.udas@capgemini.com>
2018-03-10 11:04:39 +0530

    Add files via upload

---
+++ TestCases/src/bm/petclinic/PetClinic.java
@@ -0,0 +1,40 @@
+package bm.petclinic;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class PetClinic {
+	public static WebDriver driver;
+	@BeforeSuite
+
+	public void setup(){
+		System.setProperty("webdriver.chrome.driver", "D:\\Users\\maudas\\Downloads\\chromedriver_win32 (4)\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://shoppingcart.r6fzqtkyvm.us-west-2.elasticbeanstalk.com");
+		
+	}
+
+	@Test
+	public void getTitle(){
+		String title=driver.getTitle();
+		if(title.equals("PetClinic :: a Spring Framework demonstration")){
+			System.out.println("Application is working properly");
+		}
+		
+		else{
+			System.out.println("Issue with application ");
+		}
+		
+		//driver.findElement(By.xpath("//div[@id='main-navbar']/ul/li[3]/a")).click();
+		
+	}
+}
