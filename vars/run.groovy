4c37bd1858c1cefd1d0cf0c206beab44cbe02cba
mayureshudas <mayuresh.udas@capgemini.com>
2020-07-30 19:15:53 +0530

    Update run.groovy

--- vars/run.groovy
+++ vars/run.groovy
@@ -1,8 +1,8 @@
 def runproject(){
-       sh "sudo service tomcat stop"
-       sh "sudo cd /target"
-       sh "sudo cp -fR petclinic.war /var/lib/tomcat/webapps"
-       sh "sudo service tomcat start"
+       sh "service tomcat stop"
+       sh "cd /target"
+       sh "cp -fR petclinic.war /var/lib/tomcat/webapps"
+       sh "service tomcat start"
        
 }
 return this
