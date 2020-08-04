a148e592098450542f65f496657419e0269df446
mayureshudas <mayuresh.udas@capgemini.com>
2020-08-04 19:19:33 +0530

    Update run.groovy

--- vars/run.groovy
+++ vars/run.groovy
@@ -2,7 +2,6 @@ def runproject(){
        sh "whoami"
        sh " mkdir /home/test"
        sh "cp -fR target/petclinic.war /var/lib/tomcat/webapps"
-      
        sh "service tomcat restart"
        
 }
